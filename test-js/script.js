1d95b3ecb9612449003fc65f6a5289133b9b7f0b
Даниил <156006330+0hz0-code@users.noreply.github.com>
2026-01-14 18:53:33 +0600

    Add files via upload

---
+++ test-js/script.js
@@ -0,0 +1,9 @@
+let title = 'Урок 1';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 12000;
+let rollback = 5;
+let fullPrice = 40000;
+let adaptive = true;
+
+alert('Привет! Это первое домашнее задание.');
+console.log('Скрипт успешно подключен и работает!');
